1ae78b1371995ddad1a0275e17e63bc0cf1c2594
Bilge <bilge.ulusay@expleogroup.com>
2023-05-25 12:53:42 +0200

    Enable Django admin for user profile

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -26,7 +26,7 @@ class UserProfileManager(BaseUserManager):
         
         user.is_superuser = True
         user.is_staff = True
-        user.saver(using= self._db)
+        user.save(using= self._db)
 
         return user
 
